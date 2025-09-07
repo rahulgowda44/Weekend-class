ec509ea88ffc93e8f0ec2ba37fccfe274b34bd9e
rahulgowda44 <rahulgowda44@gmail.com>
2025-09-07 12:02:38 +0530

    main code

---
+++ src/login_page/login.java
@@ -0,0 +1,14 @@
+package login_page;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class login 
+
+{
+	  @Test
+	  public void test1()
+	  {
+		  Reporter.log("Hello");
+	  }
+	}
